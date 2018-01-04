18a85ed9e4b39dcd8705907af7f0efe1a19ce877
Shanon McQuay <shanonmcquay@gmail.com>
2018-01-04 17:15:47 +1100

    Allow user data to be supplied as commandline arguments.

--- diesel_query/src/bin/create_user.rs
+++ diesel_query/src/bin/create_user.rs
@@ -1,15 +1,27 @@
 extern crate diesel_query;
 extern crate diesel;
+extern crate clap;
 
 use self::diesel_query::*;
 use self::diesel_query::models::*;
 
+use clap::{Arg, App};
+
 fn main() {
+   let matches = App::new("create_user")
+                      .arg(Arg::with_name("name")
+                           .required(true)
+                           .takes_value(true))
+                      .arg(Arg::with_name("email")
+                           .required(true)
+                           .takes_value(true))
+                      .get_matches();
+
     let connection = establish_connection();
 
     let user = create_user(&connection, NewUser {
-        name:  "Shanon",
-        email: "shanonmcquay@gmail.com"
+        name:  matches.value_of("name").unwrap(),
+        email: matches.value_of("email").unwrap(),
     });
 
     println!("Created user: {}", user.id);
