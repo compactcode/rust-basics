34fe443542e4092ac023eb0307c89c822080e21b
Shanon McQuay <shanonmcquay@gmail.com>
2018-01-04 16:02:54 +1100

    Add a users table and a way to query it.

---
+++ diesel_query/src/bin/list_users.rs
@@ -0,0 +1,25 @@
+extern crate diesel_query;
+extern crate diesel;
+
+use self::diesel_query::*;
+use self::diesel_query::models::*;
+use self::diesel::prelude::*;
+
+fn main() {
+    use diesel_query::schema::users::dsl::*;
+
+    let connection = establish_connection();
+
+    let results = users
+        .limit(5)
+        .load::<User>(&connection)
+        .expect("Error loading users");
+
+    println!("Displaying {} users", results.len());
+
+    for result in results {
+        println!("{}", result.name);
+        println!("----------\n");
+        println!("{}", result.email);
+    }
+}
