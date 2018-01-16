f7d97f2b0096cc85f13b0f6b3ceb5dc106453b16
Shanon McQuay <shanonmcquay@gmail.com>
2018-01-16 22:41:49 +1100

    Convert the batch query into an iterator.

--- diesel_query/src/bin/list_users_batched.rs
+++ diesel_query/src/bin/list_users_batched.rs
@@ -1,47 +1,79 @@
 extern crate diesel_query;
 extern crate diesel;
 
-use self::diesel_query::*;
-use self::diesel_query::models::{User};
 use self::diesel::prelude::*;
+
+use self::diesel_query::{establish_connection};
+use self::diesel_query::models::{User};
 
 fn main() {
     process().unwrap();
 }
 
 fn process() -> Result<(), diesel::result::Error> {
-    use diesel_query::schema::users::dsl::*;
+    for batch in BatchLoad::new() {
+        println!("Displaying batch {} ", batch.number);
 
-    let connection = establish_connection();
-
-    let batch_size = 5;
-
-    let mut load  = true;
-    let mut batch = 0;
-    let mut start = 0;
-
-    while load {
-        batch = batch + 1;
-
-        let batch_results = users
-            .order(id.asc())
-            .filter(id.gt(start))
-            .limit(batch_size)
-            .load::<User>(&connection)?;
-
-        if let Some(last) = batch_results.last() {
-            println!("Displaying batch {} ", batch);
-
-            for result in &batch_results {
-                println!("{}, {}, {}", result.id, result.name, result.email);
-            }
-
-            start = last.id;
-            load = true;
-        } else {
-            load = false;
+        for user in batch.data {
+            println!("{}, {}, {}", user.id, user.name, user.email);
         }
     }
 
     Ok(())
+}
+
+struct BatchLoad {
+    batch_size: i64,
+    start_id: i32,
+    current_batch: u32,
+    connection: PgConnection
+}
+
+struct Batch<T> {
+    number: u32,
+    data: Vec<T>
+}
+
+impl BatchLoad {
+    pub fn new() -> Self {
+        BatchLoad {
+            batch_size: 5,
+            start_id: 0,
+            current_batch: 0,
+            connection: establish_connection()
+        }
+    }
+}
+
+// I'm interested in figuiring out if this can be made generic so it can be used across any table.
+impl Iterator for BatchLoad {
+    type Item = Batch<User>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        use diesel_query::schema::users::dsl::*;
+
+        self.current_batch = self.current_batch + 1;
+
+        let data = users
+            .order(id.asc())
+            .filter(id.gt(self.start_id))
+            .limit(self.batch_size)
+            .load::<User>(&self.connection)
+            .expect(
+                &format!("Failed to load batch: {}", &self.current_batch)
+            );
+
+        if let &Some(last_id) = &data.last().map(|last| last.id) {
+            self.start_id = last_id;
+
+            Some(
+                Batch {
+                    number: self.current_batch,
+                    data
+                }
+            )
+        } else {
+            None
+        }
+    }
 }
--- diesel_query/src/lib.rs
+++ diesel_query/src/lib.rs
@@ -2,6 +2,7 @@
 extern crate diesel;
 extern crate dotenv;
 
+
 pub mod schema;
 pub mod models;
 
