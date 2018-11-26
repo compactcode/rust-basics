d5720c00f2b24af5b444c6dd38f94e5a2a9e93e0
Shanon McQuay <shanonmcquay@gmail.com>
2018-11-26 20:20:14 +1100

    Add template serving to the rocket application.

--- rocket_http/README.md
+++ rocket_http/README.md
@@ -5,7 +5,7 @@ I want to see what is involved in writing a web application using rocket.
 ## Goals
 
 - [x] Render HTML File
-- [ ] Render HTML Template
+- [x] Render HTML Template
 - [ ] Query String
 - [ ] Form Data
 - [ ] Form Validation
--- rocket_http/src/main.rs
+++ rocket_http/src/main.rs
@@ -1,10 +1,26 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-extern crate rocket;
+use std::collections::HashMap;
+
+#[macro_use] extern crate rocket;
 extern crate rocket_contrib;
 
 use rocket_contrib::serve::StaticFiles;
+use rocket_contrib::templates::Template;
 
+#[get("/")]
+fn index() -> Template {
+    let context: HashMap<&str, &str> = [("title", "Hello Templated Rocket!")].iter().cloned().collect();
+
+    Template::render("index", context)
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/public", StaticFiles::from("public"))
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+}
 fn main() {
-    rocket::ignite().mount("/public", StaticFiles::from("public")).launch();
+    rocket().launch();
 }
